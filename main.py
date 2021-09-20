83cdad1676b4dd3f90e1286ab30f293684940bf6
Rümeysa yük <19110131041@ogr.ksu.edu.tr>
2021-09-21 00:39:54 +0300

    .

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 from nltk.tokenize import word_tokenize, sent_tokenize
 import re  # Regex için gerekli modul
 import string
-import pandas as pd
 from nltk.corpus import stopwords
 
 f = open("jokes.csv", "r", encoding="utf8")  # r =Read   utf8 turkçe karakterlerle çalışcağımızı belirttik.
-text = f.read()
-# text = sent_tokenize(text)
-text = word_tokenize(text)
+data = f.read()
 
-punctuationSet = string.punctuation
-text = "".join(list(map(lambda x: x if x not in punctuationSet else " ", text)))
-text = re.sub("[0-9]+", "", text)
-text = text.lower()
-text = text.replace("\n ", " ")
-ineffectiveElementSet = stopwords.words("english")
-text = " ".join([i for i in text.split() if i not in ineffectiveElementSet])
-text = " ".join([i for i in text.split() if len(i) > 1])
-text = re.sub("'(\w+)", "", text)
-print(text)
+
+def clean(text):
+    punctuation_set = string.punctuation
+    ineffective_element_set = stopwords.words("english")
+    text = sent_tokenize(text, "english")
+    text = "".join(list(map(lambda x: x if x not in punctuation_set else " ", text)))
+    text = re.sub("[0-9]+", "", text)
+    text = text.lower()
+    text = text.replace("\n ", " ")
+    text = " ".join([i for i in text.split() if i not in ineffective_element_set])
+    text = " ".join([i for i in text.split() if len(i) > 1])
+    text = re.sub(r'[^\w\s]', '', text)
+    text = text.replace("'", "")
+    text = text.replace("``", "")
+    return text
+
+
+print(clean(data))
