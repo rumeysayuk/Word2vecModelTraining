37e575eb0ba99cbd97f7bb0e51650a3f254c9762
Rümeysa yük <19110131041@ogr.ksu.edu.tr>
2021-09-20 18:01:54 +0300

    .

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
-import pandas as pd  # veri işleme, CSV dosyaları
+from nltk.tokenize import word_tokenize, sent_tokenize
 import re  # Regex için gerekli modul
 import string
-import nltk
+import pandas as pd
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
-text = open("jokes.csv", "r", encoding="utf8")
+f = open("jokes.csv", "r", encoding="utf8")  # r =Read   utf8 turkçe karakterlerle çalışcağımızı belirttik.
+text = f.read()
+# text = sent_tokenize(text)
+text = word_tokenize(text)
 
-
-# text = re.sub("'(\w+)", "", text)
-def cleanData(data):
-    punctuationSet = string.punctuation
-    text = word_tokenize(data)
-    data = data.lower()
-    data = data.replace("\n ", " ")
-    return data
-
-
-#
-# print(word_tokenize(text))
+punctuationSet = string.punctuation
 text = "".join(list(map(lambda x: x if x not in punctuationSet else " ", text)))
-
+text = re.sub("[0-9]+", "", text)
+text = text.lower()
+text = text.replace("\n ", " ")
+ineffectiveElementSet = stopwords.words("english")
+text = " ".join([i for i in text.split() if i not in ineffectiveElementSet])
+text = " ".join([i for i in text.split() if len(i) > 1])
+text = re.sub("'(\w+)", "", text)
 print(text)
-# print(text)
